e9ef34c7679b5d47f0ecd233d08c8c1d67346f21
Nurin <nurinfazil@hotmail.com>
2020-05-30 16:38:17 -0400

    Implemented saving high scores in local storage

--- end.js
+++ end.js
@@ -3,13 +3,33 @@ const saveScoreBtn = document.getElementById("saveScoreBtn");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 const finalScore = document.getElementById("finalScore");
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+const MAX_HIGH = 5;
+
 finalScore.innerText = mostRecentScore;
 
-username.addEventListener('keyup', () => {
+username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = (e) => {
+saveHighScore = e => {
     console.log("clicked");
     e.preventDefault();
-}
+
+
+const score = {
+    score: mostRecentScore, 
+    name: username.value
+};
+
+highScores.push(score);
+
+// Sorts the array by score
+highScores.sort( (a,b) => b.score - a.score);
+
+highScores.splice(5);
+
+localStorage.setItem("highScores", JSON.stringify(highScores));
+window.location.assign("index.html");
+};
